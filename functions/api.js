e1079efbe17f38a9074f0bf0d806482820a8c066
Sundar <109157207+Sundar-600@users.noreply.github.com>
2025-07-26 16:54:10 +0530

    fix : hello/

--- functions/api.js
+++ functions/api.js
@@ -4,7 +4,12 @@ import serverless from "serverless-http";
 const app = express();
 
 app.get("/hello", (req, res) => {
-  res.json({ message: "Site is working now" });
+  return res.json({ message: "Site is working now" });
 });
 
-export const handler = serverless(app);
+const handler = serverless(app);
+
+module.exports.handler = async (event, context) => {
+	const result = await handler(event,context);
+	return result;
+};
